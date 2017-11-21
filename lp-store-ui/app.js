d1377dacccd53c03aa9708ad7d40edc1f21b7006
Alain Chautard <achautard@gmail.com>
2017-11-21 11:19:35 -0800

    Solution for lab #5

--- lp-store-ui/app.js
+++ lp-store-ui/app.js
@@ -8,5 +8,12 @@ app.controller('StoreController', ['$scope','$http', function($scope, $http) {
     $scope.addToCart = function(id) {
         $http.put("http://localhost:8000/cart/" + id).then(result => alert("Item added to your cart"));
     }
+
+    $scope.removeFromCart = function(id) {
+        $http.delete("http://localhost:8000/cart/" + id).then(result => {
+            $http.get("http://localhost:8000/cart").then(result => $scope.cartItems = result.data);
+            alert("Item removed from your cart");
+        });
+    }
 }]);
 
