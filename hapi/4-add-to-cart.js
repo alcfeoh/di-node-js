6c8a3b513ce1dd4de9081ca0fe1b02f0f660ce8f
Alain Chautard <achautard@gmail.com>
2017-11-21 10:56:52 -0800

    Solution for lab #4

---
+++ hapi/4-add-to-cart.js
@@ -0,0 +1,58 @@
+const Hapi = require('hapi');
+const fs = require('fs');
+const Boom = require('boom');
+const Joi = require('joi');
+
+let plates = JSON.parse(outputFile());
+let cart = [];
+
+// Create a server with a host and port
+const server = new Hapi.Server({
+    debug: { request: ['error'] },
+    connections: {
+        routes: {cors: true}
+    }
+});
+server.connection({ host: 'localhost', port: 8000 });
+
+server.route({
+    method: 'GET',  path:'/data',
+    handler: function (request, reply) {
+        return reply(JSON.stringify(plates));
+    }
+});
+
+server.route({
+    method: 'PUT',  path:'/cart/{id}',
+    config: {
+        validate: {
+            params: (params, options, next) => {
+                let idExists = plates.find(plate => plate._id == params.id);
+                next(! idExists);
+            }
+        }
+    },
+    handler: function (request, reply) {
+        cart.push(request.params.id);
+        console.log(cart);
+        return reply(request.params.id + ' added to the cart');
+    }
+});
+
+server.route({
+    method: 'GET',  path:'/{p*}',
+    handler: function (request, reply) {
+        request.log(['error', 'bad_request'], request.url.pathname);
+        return reply(Boom.notFound('Nothing to see here'));
+    }
+});
+
+// Start the server
+server.start((err) => {
+    console.log('Server running at:', server.info.uri);
+});
+
+function outputFile() {
+    return fs.readFileSync('../data/plates.json', 'utf8');
+}
+
--- lp-store-ui/app.js
+++ lp-store-ui/app.js
@@ -2,5 +2,11 @@ var app = angular.module("store-app", []);
 
 app.controller('StoreController', ['$scope','$http', function($scope, $http) {
     $http.get("http://localhost:8000/data").then(result => $scope.plates = result.data);
+
+    $http.get("http://localhost:8000/cart").then(result => $scope.cartItems = result.data);
+
+    $scope.addToCart = function(id) {
+        $http.put("http://localhost:8000/cart/" + id).then(result => alert("Item added to your cart"));
+    }
 }]);
 
