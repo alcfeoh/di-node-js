6edf7ba22fc3929c6ed4184507b24f78f4139396
Alain Chautard <achautard@gmail.com>
2017-11-20 17:10:53 -0800

    Solution for lab #3

--- lp-store-ui/app.js
+++ lp-store-ui/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("store-app", []);
 
 app.controller('StoreController', ['$scope','$http', function($scope, $http) {
-    $http.get("http://localhost:8080/plates.json").then(result => $scope.plates = result.data);
+    $http.get("http://localhost:8000/data").then(result => $scope.plates = result.data);
 }]);
 
