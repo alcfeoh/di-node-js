815fbe7cf66c29ca630888cb921e4625f597d4ad
Alain Chautard <achautard@gmail.com>
2018-01-01 17:03:26 -0800

    Updated lab 3 for Express

---
+++ express/solutions/3-cors-enabled.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+const port = 8000;
+
+// Enabling CORS
+app.use(cors());
+
+function logErrors(err ,req, res, next){
+    console.error(err);
+    res.status(404).send('Nothing to see here');
+}
+
+app.get('/', (request, resp) => {
+    resp.send('<h1>Hello Express World!</h1>')
+});
+
+// We can serve static content too!
+app.use('/data', express.static('../data', {index: 'plates.json'}));
+
+app.get('*', (request, resp, next) => {
+    throw new Error("Unknown URL: "+ request.originalUrl);
+});
+
+// Middleware for error handling
+app.use(logErrors);
+
+app.listen(port, (err) => {
+    console.log(`server listening on ${port}`)
+});
+
+
