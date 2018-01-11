dedee3b8ed96556e9cc88e0032f781d08c6480da
Alain Chautard <alain@interstate21.com>
2018-01-11 15:28:11 -0800

    New lab 6 with Hapi

---
+++ 6-hapi.js
@@ -0,0 +1,37 @@
+const Hapi = require('hapi');
+const fs = require('fs');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({ host: 'localhost', port: 8000 });
+
+// Add the routes
+server.route({
+    method: 'GET',  path:'/hello',
+    handler: function (request, reply) {
+        return reply('Hello Hapi World!');
+    }
+});
+
+server.route({
+    method: 'GET',  path:'/data',
+    handler: function (request, reply) {
+        return reply(outputFile());
+    }
+});
+
+server.route({
+    method: 'GET',  path:'/{p*}',
+    handler: function (request, reply) {
+        return reply('Nothing to see here');
+    }
+});
+
+// Start the server
+server.start((err) => {
+    console.log('Server running at:', server.info.uri);
+});
+
+function outputFile() {
+    return fs.readFileSync('./data/plates.json', 'utf8');
+}
